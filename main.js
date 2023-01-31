3239a7cd9248d60cc99e3c934f7ff3c0999388c8
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-01-31 16:31:44 +0200

    ..
    ..

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 let shop = document.getElementById("shop");
+let cart =document.getElementById('cart')
+let numProduct =0
 // create array inside it obs
+
+let arrItem = JSON.parse(localStorage.getItem('data')) || []
+
+
 let arr = [
   {
     id: 1,
@@ -58,7 +64,7 @@ for(let product =0 ; product< arr.length ;product++){
 }
 
 
- let arrItem = []
+
 function increment(id){
 let search = arrItem.find((x) => x.id === id)
 if(search === undefined){
@@ -70,8 +76,8 @@ if(search === undefined){
   search.item +=1
 
 }
-
-console.log(arrItem)
+localStorage.setItem("data" ,JSON.stringify(arrItem))
+update(id)
 
 }
 
@@ -82,7 +88,22 @@ function decrement(id){
   let search = arrItem.find((x) => x.id === id)
 if(search.item ===0) return 
 else search.item  -=1 
+update(id)
+localStorage.setItem("data" ,JSON.stringify(arrItem))
 
   
-  console.log(arrItem)
+
 }
+ function update(id){
+  let search = arrItem.find((x) => x.id === id)
+  let el =document.getElementById(id)
+  el.innerHTML=search.item
+  calc()
+
+ }
+ function calc(){
+  numProduct  = arrItem.map( e => e.item).reduce((e,a)  =>{
+    return e+a
+  })
+  cart.innerHTML =numProduct
+ }
